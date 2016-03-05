fee3ebc2645f6ba613c78d72606b8be4180b964d
tomas caldas <up201404990@fe.up.pt>
2016-03-05 18:13:51 +0000

    1.4 finalizado e class sword

--- src/Interface/Sword.java
+++ src/Interface/Sword.java
@@ -1,5 +1,22 @@
 package Interface;
 
 public class Sword {
-
+	private int x,y ;
+	private boolean dragonAndSword;
+	
+	public Sword(int x, int y){
+		this.x = x;
+		this.y = y;
+		dragonAndSword = false;
+	}
+	
+	public boolean getDragonAndSword(){
+		return dragonAndSword;
+	}
+	
+	public void setDragonAndSword(boolean dands){
+		dragonAndSword = dands;
+	}
+	
+	
 }
