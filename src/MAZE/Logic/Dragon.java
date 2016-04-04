285f216b69e48967ab45aabc76d1392f27378602
Ines Ferreira <inesferreira789@gmail.com>
2016-04-04 18:45:25 +0100

    bomojyvguhy7

--- src/MAZE/Logic/Dragon.java
+++ src/MAZE/Logic/Dragon.java
@@ -5,16 +5,24 @@ import java.util.Scanner;
 public class Dragon {
 	
 	public enum Type {STATIONARY, RANDOM, SLEEPING}
-	//o dragao pode nao se mover, pode mover-se para uma das casas adjacente aleatoriamente ou pode ser aleatorio e dormir.
+	
 	
 	int x, y;
 	private boolean dragonAlive;
 	private Type type;
 	private boolean dragonAsleep;
 	
+	/**
+	 * Constrói um dragão
+	 */
 	public Dragon(){
 	}
 	
+	/**
+	 * Constrói um dragão vivo, acordado
+	 * @param x
+	 * @param y
+	 */
 	public Dragon (int x, int y){
 		dragonAlive = true;
 		this.x = x;
@@ -22,36 +30,68 @@ public class Dragon {
 		dragonAsleep = false;
 	}
 	
+	/**
+	 * Verifica o estado de vida do dragão
+	 * @return true se o dragão estiver vivo e false se estiver morto
+	 */
 	public boolean getDragonAlive(){
 		return dragonAlive;
 	}
 	
+	/**
+	 * Define o estado de vida do drgão
+	 * @param alive
+	 */
 	public void setDragonAlive(boolean alive){
 		dragonAlive = alive;
 	}
-
+	
+	/**
+	 * Gera um número aleatório de 1 a 5
+	 * @return o número gerado aleatoriamente
+	 */
 	public int randomDragon(){
 		Random r = new Random();	
 		int dmove = r.nextInt(4) +1; 	
 		return dmove;
 	}
 	
+	/**
+	 * Retorna a posição x
+	 * @return posição x 
+	 */
 	public int getX(){
 		return x;
 	}
 	
+	/**
+	 * Retorna a posição y
+	 * @return y
+	 */
 	public int getY(){
 		return y;
 	}
 	
+	/**
+	 * Define a posição em x para newX
+	 * @param newX
+	 */
 	public void setX(int newX){
 		x = newX;
 	}
 	
+	/**
+	 * Define a posição em y para newY
+	 * @param newY
+	 */
 	public void setY(int newY){
 		y = newY;
 	}
 	
+	/**
+	 * Verifica 
+	 * @return true se o dragão estiver a dormir e false se estiver acordado
+	 */
 	public boolean getDragonAsleep(){
 		return dragonAsleep;
 	}
