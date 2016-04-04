ec894f7609bd12658d4a521140ec74e299ea2600
Ines Ferreira <inesferreira789@gmail.com>
2016-04-04 19:26:03 +0100

    Pintando acho

--- src/MAZE/Logic/Sword.java
+++ src/MAZE/Logic/Sword.java
@@ -4,34 +4,64 @@ public class Sword {
 
 	private int x,y ;
 	private boolean dragonAndSword;
+	/**
+	 * @brief Constroi e inicializa a espada numa localização específica
+	 * @param x
+	 * @param y
+	 */
+	
 	
 	public Sword(int x, int y){
 		this.x = x;
 		this.y = y;
 		dragonAndSword = false;
 	}
+	/**
+	 * @brief Constroi e inicializa a espada
+	 */
+	public Sword(){}
 	
+	/**
+	 * @brief Retorna um booleano em relação à sobreposição do dragão e a espada
+	 * @return novo valor do booleano
+	 */
 	public boolean getDragonAndSword(){
 		return dragonAndSword;
 	}
 	
+	/**
+	 * @brief Altera o valor do booleano em relação à sobreposição do dragão e da espada
+	 * @param dands
+	 */
 	public void setDragonAndSword(boolean dands){
 		dragonAndSword = dands;
 	}	
-	
+	/**
+	 * @brief retorna o valor de X da espada
+	 * @return valor de x
+	 */
 	public int getX(){
 		return x;
 	}
-	
+	/**
+	 * @brief retorna o valor de Y da espada
+	 * @return valor de y
+	 */
 	public int getY(){
 		return y;
 	}
-	
+	/**
+	 * @brief altera o valor de X da espada
+	 * @param newX
+	 */
 	public void setX(int newX){
 		x = newX;
 	}
-	
+	/**
+	 * @brief altera o valor de y da espada
+	 * @param newY
+	 */
 	public void setY(int newY){
 		y = newY;
-	}
+	}	
 }
